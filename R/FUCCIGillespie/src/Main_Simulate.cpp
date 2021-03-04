fa3a17a3444c1d58010b194735e76884573f6809
Michael Carr <69828662+michaelcarr-stats@users.noreply.github.com>
2021-03-04 11:24:16 +1000

    Update Main_Simulate.cpp

--- R/FUCCIGillespie/src/Main_Simulate.cpp
+++ R/FUCCIGillespie/src/Main_Simulate.cpp
@@ -212,6 +212,12 @@ Rcpp::List Main_Simulate(arma::vec theta, Rcpp::List SetupVars, double T_record,
       }
       
       SummaryStatsData = GenerateSummaryStatData(CellTracking, SummaryStatsData, ntrack, T_record, t, Nred, Nyellow, Ngreen, RowPosCell, ColPosCell, CellSelected, CellSelectedStart, MigPosition, rowIndex, columnIndex, migFailed, transID, delta, simuIndex, simuNum, domain, domain_x);
+      
+      if (!migFailed) {
+        RowPosCell(cellID) = MigPosition(0); //update location of tracked cells
+        ColPosCell(cellID) = MigPosition(1); //update location of tracked cells
+      }
+      
       //reset transition/migration indicators
       transID = 0;
       migFailed = 1;
