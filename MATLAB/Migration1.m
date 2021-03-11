9febec958f9a95e5da985a17945b758888596d43
Michael Carr <69828662+michaelcarr-stats@users.noreply.github.com>
2021-03-11 11:23:44 +1000

    Add files via upload

--- MATLAB/Migration1.m
+++ MATLAB/Migration1.m
@@ -5,110 +5,110 @@ P = rand; %Genearte a random number between 0 and 1
 
 %{
 Target Sites arround lattice site L.
-   ___  ___
-  / 3 \/ 5 \
-  \___/\___/__
-/ 1 \/ L \/ 2 \
-\___/\___/\___/ 
-  / 4 \/ 6 \
-  \___/\___/
+       ___  ___              _______
+      / 3 \/ 5 \            | 3 | 5 |
+      \___/\___/__          |___|___|___
+    / 1 \/ L \/ 2 \    =>   | 1 | L | 2 |
+    \___/\___/\___/         |___|___|___|
+      / 4 \/ 6 \            | 4 | 6 |
+      \___/\___/            |___|___|
 
 %}
 
 if BC==1
     if P<1/6 %Target site 1
-        if rowIndex==1
-            MigRow = rowNum;
+        if columnIndex == 1
+            MigColumn = columnNum;
         else
-            MigRow = MigRow - 1;
+            MigColumn = columnIndex - 1;
         end
     elseif P>=1/6 && P<1/3 %Target site 2
-        if rowIndex==1
-            MigRow = MigRow - 1 + rowNum;
-        else
-            MigRow = MigRow - 1;
-        end
-        if columnIndex==columnNum-1
+        if columnIndex == columnNum
             MigColumn = 1;
-        elseif columnIndex<columnNum-1
-            MigColumn = MigColumn + 1;
+        else
+            MigColumn = columnIndex + 1;
         end
     elseif P>=1/3 && P<1/2 %Target site 3
-        if columnIndex==columnNum-1
-            MigColumn = 1;
-        elseif columnIndex<columnNum-1
-            MigColumn = MigColumn + 1;
+        if rowIndex == 1
+            MigRow = rowNum;
+        else
+            MigRow = rowIndex - 1;
+        end
+        if columnIndex == 1
+            MigColumn = ColumnNum;
+        else
+            MigColumn = columnIndex - 1;
         end
     elseif P>=1/2 && P<2/3 %Target site 4
-        if rowIndex==rowNum
+        if rowIndex == rowNum
             MigRow = 1;
         else
-            MigRow = MigRow + 1;
+            MigRow = rowIndex + 1;
         end
-        if columnIndex==columnNum-1
-            MigColumn = 1;
-        elseif columnIndex<columnNum-1
-            MigColumn = MigColumn + 1;
+        if columnIndex == 1
+            MigColumn = columnNum;
+        else
+            MigColumn = columnIndex - 1;
         end
     elseif P>=2/3 && P<5/6 %Target site 5
-        if rowIndex==rowNum
-            MigRow = 1;
+        if rowIndex == 1
+            MigRow = rowNum;
         else
-            MigRow = MigRow + 1;
+            MigRow = rowIndex - 1;
         end
     elseif P>=5/6 %Target site 6
-        if columnIndex==1
-            MigColumn = columnNum - 1;
+        if rowIndex == rowNum
+            MigRow = 1;
         else
-            MigColumn = MigColumn - 1;
+            MigRow = rowIndex + 1;
         end
     end
 elseif BC==2
     if P<1/6 %Target site 1
-        if rowIndex==1
-            MigRow = rowIndex;
+        if columnIndex == 1
+            MigColumn = 1;
         else
-            MigRow = MigRow - 1;
+            MigColumn = columnIndex - 1;
         end
     elseif P>=1/6 && P<1/3 %Target site 2
-        if rowIndex==1
-            MigRow = rowIndex;
+        if columnIndex == columnNum
+            MigColumn = columnNum;
+        else
+            MigColumn = columnIndex + 1;
+        end
+    elseif P>=1/3 && P<1/2 %Target site 3
+        if rowIndex == 1
+            MigRow = 1;
+        else
+            MigRow = rowIndex - 1;
+        end
+        if columnIndex == 1
+            MigColumn = 1;
+        else 
+            MigColumn = columnIndex - 1;
+        end
+    elseif P>=1/2 && P<2/3 %Target site 4
+        if rowIndex == rowNum
+            MigRow = rowNum;
+        else
+            MigRow = rowIndex + 1;
+        end
+        if columnIndex == 1
+            MigColumn = 1;
+        else 
+            MigColumn = columnIndex - 1;
+        end
+    elseif P>=2/3 && P<5/6 %Target site 5
+        if rowIndex == 0 
+            MigRow = 0;
         else
             MigRow = MigRow - 1;
         end
-        if columnIndex==columnNum-1
-            MigColumn = columnIndex;
-        elseif columnIndex<columnNum-1
-            MigColumn = MigColumn + 1;
-        end
-    elseif P>=1/3 && P<1/2 %Target site 3
-        if columnIndex==columnNum-1
-            MigColumn = columnIndex;
-        elseif columnIndex<columnNum-1
-            MigColumn = MigColumn + 1;
-        end
-    elseif P>=1/2 && P<2/3 %Target site 4
-        if rowIndex==rowNum
-            MigRow = rowIndex;
-        else
-            MigRow = MigRow + 1;
-        end
-        if columnIndex==columnNum-1
-            MigColumn = columnIndex;
-        elseif columnIndex<columnNum-1
-            MigColumn = MigColumn + 1;
-        end
-    elseif P>=2/3 && P<5/6 %Target site 5
-        if rowIndex==rowNum
-            MigRow = rowIndex;
-        else
-            MigRow = MigRow + 1;
-        end
     elseif P>=5/6 %Target site 6
-        if columnIndex==1
-            MigColumn = columnIndex;
+        if rowIndex == rowNum 
+            MigRow = rowNum;
         else
-            MigColumn = MigColumn - 1;
+            MigRow = MigRow + 1;
         end
     end    
 end
--- MATLAB/Migration2.m
+++ MATLAB/Migration2.m
@@ -5,122 +5,110 @@ P = rand; %Genearte a random number between 0 and 1
 
 %{
 Target Sites arround lattice site L.
-   ___  ___
-  / 3 \/ 5 \
-  \___/\___/__
-/ 1 \/ L \/ 2 \
-\___/\___/\___/ 
-  / 4 \/ 6 \
-  \___/\___/
+       ___  ___                  _______
+      / 3 \/ 5 \                | 3 | 5 |
+      \___/\___/__           ___|___|___|
+    / 1 \/ L \/ 2 \    =>   | 1 | L | 2 |
+    \___/\___/\___/         |___|___|___|
+      / 4 \/ 6 \                | 4 | 6 |
+      \___/\___/                |___|___|
 
 %}
 
 if BC==1
     if P<1/6 %Target site 1
-        if rowIndex==1
-            MigRow = rowNum;
-        else
-            MigRow = MigRow - 1;
-        end
-        if columnIndex==1
-            MigColumn = columnNum - 1;
-        else
-            MigColumn = MigColumn - 1;
-        end
-    elseif P>=1/6 && P<1/3 %Target site 2
-        if rowIndex==1
-            MigRow = rowNum;
-        else
-            MigRow = MigRow - 1;
-        end
-        if columnIndex==columnNum
-            MigColumn = 1;
-        end
-    elseif P>=1/3 && P<1/2 %Target site 3
-        if columnIndex==columnNum
-            MigColumn = 1;
-        else
-            MigColumn = MigColumn + 1;
-        end
-    elseif P>=1/2 && P<2/3 %Target site 4
-        if rowIndex==rowNum
-            MigRow = 1;
-        else
-            MigRow = MigRow + 1;
-        end
-        if columnIndex==columnNum
-            MigColumn = 1;
-        end
-    elseif P>=2/3 && P<5/6 %Target site 5
-        if rowIndex==rowNum
-            MigRow = 1;
-        else
-            MigRow = MigRow + 1;
-        end
-        if columnIndex==1
-            MigColumn = columnNum - 1;
-        else
-            MigColumn = MigColumn - 1;
-        end
-    elseif P>=5/6 %Target site 6
-        if columnIndex==1
+        if columnIndex == 1
             MigColumn = columnNum;
         else
             MigColumn = MigColumn - 1;
         end
+    elseif P>=1/6 && P<1/3 %Target site 2
+        if columnIndex == columnNum
+            MigColumn = 1;
+        else
+            MigColumn = columnIndex + 1;
+        end
+    elseif P>=1/3 && P<1/2 %Target site 3
+        if rowIndex == 1
+            MigRow = rowNum;
+        else
+            MigRow = rowIndex - 1;
+        end
+    elseif P>=1/2 && P<2/3 %Target site 4
+        if rowIndex == rowNum
+            MigRow = 1;
+        else
+            MigRow = rowIndex + 1;
+        end
+    elseif P>=2/3 && P<5/6 %Target site 5
+        if rowIndex == 1
+            MigRow = rowNum;
+        else
+            MigRow = rowIndex - 1;
+        end
+        if columnIndex == columnNum
+            MigColumn = 1;
+        else
+            MigColumn = columnIndex + 1;
+        end
+    elseif P>=5/6 %Target site 6
+        if rowIndex == rowNum
+            MigRow = 1;
+        else
+            MigRow = rowIndex + 1;
+        end
+        if columnIndex == columnNum
+            MigColumn = 1;
+        else
+            MigColumn = columnIndex + 1;
+        end
     end
 elseif BC==2
-     if P<1/6 %Target site 1
-        if rowIndex==1
-            MigRow = rowIndex;
-        else
-            MigRow = MigRow - 1;
-        end
-        if columnIndex==1
-            MigColumn = columnIndex;
+    if P<1/6 %Target site 1
+        if columnIndex == 1
+            MigColumn = 1;
         else
             MigColumn = MigColumn - 1;
         end
     elseif P>=1/6 && P<1/3 %Target site 2
-        if rowIndex==1
-            MigRow = rowIndex;
+        if columnIndex == columnNum
+            MigColumn = columnNum;
         else
-            MigRow = MigRow - 1;
-        end
-        if columnIndex==columnNum
-            MigColumn = columnIndex;
+            MigColumn = columnIndex + 1;
         end
     elseif P>=1/3 && P<1/2 %Target site 3
-        if columnIndex==columnNum
-            MigColumn = columnIndex;
+        if rowIndex == 1
+            MigRow = 1;
         else
-            MigColumn = MigColumn + 1;
+            MigRow = rowIndex - 1;
         end
     elseif P>=1/2 && P<2/3 %Target site 4
-        if rowIndex==rowNum
-            MigRow = rowIndex;
+        if rowIndex == rowNum
+            MigRow = rowNum;
         else
-            MigRow = MigRow + 1;
-        end
-        if columnIndex==columnNum
-            MigColumn = columnIndex;
+            MigRow = rowIndex + 1;
         end
     elseif P>=2/3 && P<5/6 %Target site 5
-        if rowIndex==rowNum
-            MigRow = rowIndex;
+        if rowIndex == 1
+            MigRow = 1;
         else
-            MigRow = MigRow + 1;
+            MigRow = rowIndex - 1;
         end
-        if columnIndex==1
-            MigColumn = columnIndex;
+        if columnIndex == columnNum
+            MigColumn = columnNum;
         else
-            MigColumn = MigColumn - 1;
+            MigColumn = columnIndex + 1;
         end
     elseif P>=5/6 %Target site 6
-        if columnIndex==1
-            MigColumn = columnIndex;
+        if rowIndex == rowNum
+            MigRow = rowNum;
         else
-            MigColumn = MigColumn - 1;
+            MigRow = rowIndex + 1;
+        end
+        if columnIndex == columnNum
+            MigColumn = columnNum;
+        else
+            MigColumn = columnIndex + 1;
         end
     end
 end
