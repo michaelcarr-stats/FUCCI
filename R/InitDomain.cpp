ab24c73f588eac006eb87a861f4bb4ca33282eed
Michael Carr <69828662+michaelcarr-stats@users.noreply.github.com>
2021-04-09 15:46:59 +1000

    Added comments

--- R/InitDomain.cpp
+++ R/InitDomain.cpp
@@ -38,19 +38,19 @@ IntegerMatrix InitDomain(NumericMatrix &domain_x, int &nrow, int &ncol, double &
         int row;
         int col;
         int count = 0;
-        IntegerVector Index = Rcpp::sample(nrow*ncol,nrow*ncol,false);
-
+        
+        IntegerVector Index = Rcpp::sample(nrow*ncol,nrow*ncol,false); //randomly sample indexs
         for (int i = 0; i < nrow*ncol; i++) {
-            row = (Index(i) - 1) % nrow;
-            col = floor((Index(i) - 1)/nrow);
+            row = (Index(i) - 1) % nrow; //compute row index
+            col = floor((Index(i) - 1)/nrow); //compute column index
             if ((domain_x(row, col) >= 0 && domain_x(row, col) <= InitialL/2) || (domain_x(row, col) >= Xmax - InitialL/2 && domain_x(row, col) <= Xmax)) {
                 count += 1;
-                if (count <= countIS[0]) {
-                    domain(row, col) = 1;
-                } else if (count <= countIS[0] + countIS[1]) {
-                    domain(row, col) = 2;
-                } else if (count <= countIS[0] + countIS[1] + countIS[2]) {
-                    domain(row, col) = 3;
+                if (count <= countIS[0]) { //ensure red cell limit not reached
+                    domain(row, col) = 1; //assign red agent to location
+                } else if (count <= countIS[0] + countIS[1]) { //ensure yellow cell limit not reached
+                    domain(row, col) = 2; //assign yellow agent to location
+                } else if (count <= countIS[0] + countIS[1] + countIS[2]) { //ensure green cell limit not reached
+                    domain(row, col) = 3; //assign green agent to location
                 }
             }
         }
