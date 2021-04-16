43cc9ed93d3359020103cd9b5c662882806599f8
Michael Carr <69828662+michaelcarr-stats@users.noreply.github.com>
2021-04-16 10:13:27 +1000

    Added comments

--- MATLAB/InitialMotilitySelect.m
+++ MATLAB/InitialMotilitySelect.m
@@ -1,18 +1,18 @@
 function [RowPosCell,ColPosCell,CellSelectedIndex] = InitialMotilitySelect(ntrack, domain, domain_x, Xmax)
 
-Index = reshape(1:numel(domain_x), size(domain_x));
+Index = reshape(1:numel(domain_x), size(domain_x)); %domain indicies
 Index(domain ~= 1) = NaN; %identify positions which are not red
 
-Index = Index(~isnan(Index));
-ValidX = domain_x(Index) > 0 & domain_x(Index) < Xmax;
-Index = [Index(ValidX),domain_x(Index(ValidX)), domain_x(Index(ValidX)) < Xmax/2];
-IndexLHS = sortrows(Index(Index(:,3) == 1,:), 2, 'descend');
-IndexRHS = sortrows(Index(Index(:,3) == 0,:), 2, 'ascend');
+Index = Index(~isnan(Index)); %remove indicies which are not red
+ValidX = domain_x(Index) > 0 & domain_x(Index) < Xmax; %determine which cells are in the domain
+Index = [Index(ValidX),domain_x(Index(ValidX)), domain_x(Index(ValidX)) < Xmax/2]; % matrix with columns: Indicies in domain, x position of indicies, if on LHS of scratched region
+IndexLHS = sortrows(Index(Index(:,3) == 1,:), 2, 'descend'); %sort indicies on LHS of domain by x-position in descending order
+IndexRHS = sortrows(Index(Index(:,3) == 0,:), 2, 'ascend'); %sort indicies on LHS of domain by x-position in ascending  order
 
-CellSelectedIndex = [IndexLHS(1:ceil(ntrack/2),1);IndexRHS(1:ceil(ntrack/2),1)];
+CellSelectedIndex = [IndexLHS(1:ceil(ntrack/2),1);IndexRHS(1:ceil(ntrack/2),1)]; % select cells on leading edge
 CellSelectedIndex = CellSelectedIndex(1:ntrack); %ensure only ntrack cells are tracked
-RowPosCell = mod(CellSelectedIndex-1,size(domain_x,1))+1;
-ColPosCell = ceil(CellSelectedIndex/size(domain_x,1));
+RowPosCell = mod(CellSelectedIndex-1,size(domain_x,1))+1; %compute row index from indecies
+ColPosCell = ceil(CellSelectedIndex/size(domain_x,1)); %compute column index from indecies
         
 end
 
